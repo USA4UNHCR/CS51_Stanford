81cfb493a41395e99204d5e673e1de92fcbe2387
pessoaflavio <u4u@usaforunhcrs-MacBook-Pro.local>
2019-10-29 12:31:12 -0400

    Updates

---
+++ precache-manifest.4b3e5ce3cde73f610c03f98e21e6aa48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a2dadd1e9df974f4682",
+    "url": "/it_testenv/static/js/runtime~main.6765fd77.js"
+  },
+  {
+    "revision": "3044e16930ba9d12bd7c",
+    "url": "/it_testenv/static/js/main.042412e1.chunk.js"
+  },
+  {
+    "revision": "d9f5f93e72cec49603d9",
+    "url": "/it_testenv/static/js/2.e04f9eb5.chunk.js"
+  },
+  {
+    "revision": "3044e16930ba9d12bd7c",
+    "url": "/it_testenv/static/css/main.b14591ea.chunk.css"
+  },
+  {
+    "revision": "d9f5f93e72cec49603d9",
+    "url": "/it_testenv/static/css/2.0c85512e.chunk.css"
+  },
+  {
+    "revision": "44e103aa6c9c7c2569805247a8ca7ce3",
+    "url": "/it_testenv/index.html"
+  }
+];

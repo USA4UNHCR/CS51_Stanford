07042793334fc3a58347bec545f77ca8a0f9ec4b
pessoaflavio <u4u@usaforunhcrs-MacBook-Pro.local>
2019-10-29 12:48:03 -0400

    Updates

---
+++ precache-manifest.1ebb96f65020a99d4c60fd2d785adfb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "241568de391072222cbc",
+    "url": "/CS51_Stanford/static/js/runtime~main.4b4d182e.js"
+  },
+  {
+    "revision": "02289c3e744e4210ae5a",
+    "url": "/CS51_Stanford/static/js/main.dfb079da.chunk.js"
+  },
+  {
+    "revision": "d9f5f93e72cec49603d9",
+    "url": "/CS51_Stanford/static/js/2.e04f9eb5.chunk.js"
+  },
+  {
+    "revision": "02289c3e744e4210ae5a",
+    "url": "/CS51_Stanford/static/css/main.b14591ea.chunk.css"
+  },
+  {
+    "revision": "d9f5f93e72cec49603d9",
+    "url": "/CS51_Stanford/static/css/2.0c85512e.chunk.css"
+  },
+  {
+    "revision": "907d32b2a9ff438c17b85b134fb09f4f",
+    "url": "/CS51_Stanford/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/it_testenv/precache-manifest.4b3e5ce3cde73f610c03f98e21e6aa48.js"
+  "/CS51_Stanford/precache-manifest.1ebb96f65020a99d4c60fd2d785adfb9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/it_testenv/index.html", {
+workbox.routing.registerNavigationRoute("/CS51_Stanford/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
